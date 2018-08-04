f6600b55be48890260bec668ab6ac53686ae5509
clinyong <clinyong@gmail.com>
2018-08-04 13:50:35 +0800

    Add test case

--- test/decimal-to-ieee754.test.js
+++ test/decimal-to-ieee754.test.js
@@ -1,17 +1,24 @@
 import { test } from "ava";
 import { decimalToIEEE754 } from "../src";
 
-test("only fraction part", t => {
+test("only fraction", t => {
   t.is(
     decimalToIEEE754("0.1"),
     "0011111110111001100110011001100110011001100110011001100110011001"
   );
 });
 
-test("only integer part", t => {
+test("only integer", t => {
   t.is(
     decimalToIEEE754("1"),
     "0011111111110000000000000000000000000000000000000000000000000000"
+  );
+});
+
+test("integer with fraction", t => {
+  t.is(
+    decimalToIEEE754("1.1"),
+    "0011111111110001100110011001100110011001100110011001100110011001"
   );
 });
 
